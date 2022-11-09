271f3b3e7c4550459fb76d5b98a37da9e46fa2aa
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-11-09 10:03:49 -0500

    get user route

--- src/v1/services/user.service.js
+++ src/v1/services/user.service.js
@@ -2,6 +2,32 @@ const User = require( '../database/user.db')
 
 const getUser = async( id) => {
     const user = await User.getUser( id)
+
+    await user.populate([
+        {
+            path: 'accounts', 
+            select: { 'userID': 0}, 
+            populate:[
+                {path: 'accType'}, 
+                {path: 'currency'}, 
+                {
+                    path: 'transactions',
+                    populate: 'type'
+                }
+            ]
+        },
+        {
+            path: 'creditCards',
+            populate: {
+                path: 'transactions',
+                populate: [{path: 'transactions'}]
+            }
+        },
+        {
+            path: 'beneficiaries'
+        }
+    ])
+    
     return user
 }
 
